8f3a8085d1c712f3b8b9efc29aa6abe3affe2e54
Sasha9805 <pimenovs33@gmail.com>
2021-03-17 18:44:34 +0200

    Добавлена реализация показа спиннера и контента в компоненте случайной планеты

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-hhajkzholkh">
+      <div className="ldio-h8f9qb8gjjf">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
