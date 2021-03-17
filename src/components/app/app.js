e356e03c86a4cb72d1418c798327bdd19c63ea09
Sasha9805 <pimenovs33@gmail.com>
2021-03-17 14:36:11 +0200

    Создана структура файлов и папок, созданы и стилизованы компоненты

---
+++ src/components/app/app.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './app.css';
+
+import Header from '../header';
+import RandomPlanet from "../random-planet";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+
+const App = () => {
+
+  return (
+    <div className="app">
+
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb-2">
+
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+
+      </div>
+
+    </div>
+  );
+
+};
+
+export default App;
