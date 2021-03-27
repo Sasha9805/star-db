88bad86974e5e1b0403cd8f7dbabeedc0367d310
Sasha9805 <pimenovs33@gmail.com>
2021-03-27 16:36:26 +0200

    Добавлена валидация некоторых props с помощью библиотеки prop-types

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './row.css';
 
@@ -18,6 +19,11 @@ const Row = ({left, right}) => {
     </div>
   );
 
+};
+
+Row.propTypes = {
+  left: PropTypes.node,
+  right: PropTypes.node
 };
 
 export default Row;
