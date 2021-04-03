fddf8fd607510b34cc5a79ff1e8cd86607421b1c
Sasha9805 <pimenovs33@gmail.com>
2021-04-03 16:06:30 +0300

    Добавлена эмуляция логина и секретной страницы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,7 @@ import SwapiService from "../../services/swapi-service";
 import DummySwapiService from "../../services/dummy-swapi-service";
 import ErrorBoundary from "../error-boundary";
 import { SwapiServiceProvider } from "../swapi-service-context";
-import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
+import { PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage } from "../pages";
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import StarshipDetails from "../sw-components/starship-details";
@@ -16,7 +16,12 @@ import StarshipDetails from "../sw-components/starship-details";
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({isLoggedIn: true})
   };
 
   onServiceChange = () => {
@@ -29,6 +34,8 @@ export default class App extends Component {
   };
 
   render() {
+
+    const {isLoggedIn} = this.state;
 
     return (
       <ErrorBoundary>
@@ -58,7 +65,28 @@ export default class App extends Component {
                 render={({ match }) => {
                   const { id } = match.params;
                   return <StarshipDetails itemId={id} />;
-                }} />
+                }}
+              />
+
+              <Route
+                path="/login"
+                render={() => {
+                  return (
+                    <LoginPage
+                      isLoggedIn={isLoggedIn}
+                      onLogin={this.onLogin}
+                    />
+                  );
+                }}
+              />
+              <Route
+                path="/secret"
+                render={() => {
+                  return(
+                    <SecretPage isLoggedIn={isLoggedIn} />
+                  );
+                }}
+              />
 
             </div>
           </Router>
